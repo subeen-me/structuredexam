1de3c09107b79e92dda03101aba88f4c559f21c4
subeen-me <subeen.me@gmail.com>
2021-10-08 22:30:20 +0900

    update structured

--- src/part2/topdown/Program.java
+++ src/part2/topdown/Program.java
@@ -3,8 +3,53 @@ package part2.topdown;
 public class Program {
 
 	public static void main(String[] args) {
+
+		int[][] lottos = null;
+		int menu;
+		boolean running = true;
+		
+		while (running) {
+			menu = inputMenu();
+
+			switch (menu) {
+			case 1:
+				lottos = createLottosAuto();
+				break;
+			case 2:
+				lottos = createLottosManual();
+				break;
+			case 3:
+				printlottos(lottos);
+				break;
+			case 4:
+				running = false;
+				break;
+
+			default:
+			}
+
+		}
+
+	}
+
+	private static void printlottos(int[][] lottos) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	private static int[][] createLottosManual() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static int[][] createLottosAuto() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static int inputMenu() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
